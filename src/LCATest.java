5b6aca7928501de8b44b6ea73023fbd56cf97342
nacarney <nacarney@tcd.ie>
2020-10-14 18:51:10 +0100

    Created LCATest Class

---
+++ src/LCATest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LCATest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSearch() {
+		
+	}
+
+}
